7aff6a36b881691e61d817d28209d7a07b61f889
TJKNL <39524446+TJKNL@users.noreply.github.com>
2021-11-29 21:48:20 +0100

    update compose

--- kafka producer/admin.py
+++ kafka producer/admin.py
@@ -8,11 +8,17 @@ def delete_topics(admin, topic):
 def create_topics(admin, topic_list):
     admin.create_topics(new_topics=topic_list, validate_only=False)
 
-delete_topics(KafkaAdminClient(bootstrap_servers="34.141.229.194:9092", client_id='test'), 'test')
+delete_topics(KafkaAdminClient(bootstrap_servers="34.141.229.194:9092", client_id='test'), 'movies')
+delete_topics(KafkaAdminClient(bootstrap_servers="34.141.229.194:9092", client_id='test'), 'roles')
+#delete_topics(KafkaAdminClient(bootstrap_servers="34.141.229.194:9092", client_id='test'), 'test')
+
+
+
+
 if __name__ == '__main__':
     admin_client = KafkaAdminClient(bootstrap_servers="34.141.229.194:9092", client_id='test')  # use your VM's external IP Here!
-    topic_list = [NewTopic(name="test", num_partitions=1, replication_factor=1)]
-                  #NewTopic(name="wordcount", num_partitions=1, replication_factor=1)]
+    topic_list = [NewTopic(name="movies", num_partitions=1, replication_factor=1),
+                  NewTopic(name="roles", num_partitions=1, replication_factor=1)]
     create_topics(admin_client, topic_list)
 
 
--- kafka producer/producer_demo.py
+++ kafka producer/producer_demo.py
@@ -21,13 +21,26 @@ def error(exception):
 
 if __name__ == '__main__':
     producer = KafkaProducer(bootstrap_servers=f'{vm_ip}:9092')  # use your VM's external IP Here!
-    with open('/Users/twan/PycharmProjects/DE2/stream_data/data.csv') as f:
-        lines = f.readlines()
+    with open('/Users/twan/PycharmProjects/DE2/stream_data/movies.csv') as f:
+        lines_movies = f.readlines()
+    with open('/Users/twan/PycharmProjects/DE2/stream_data/roles.csv') as f2:
+        lines_roles = f2.readlines()
 
-    for line in lines:
-        kafka_python_producer_sync(producer, line, 'test')
-        sleep(randint(1,3))
 
+    i = 0
+
+    for i in range(0, len(lines_roles)):
+        try:
+            kafka_python_producer_sync(producer, lines_movies[i], 'movies')
+        except:
+            print('Movies out of data, nothing to stream.')
+        try:
+            kafka_python_producer_sync(producer, lines_roles[i], 'roles')
+        except:
+            print('Roles out of data, nothing to stream.')
+        i += 1
+        sleep(randint(1, 2))
     f.close()
+    f2.close()
 
 
